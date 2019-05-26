7ee15e50befaed289a8ebbcd1485370f472ac0e6
NamNX <0938229699a@gmail.com>
2019-05-26 19:52:24 +0700

    Up_FirstSource

---
+++ src/main/java/com/example/springboot/SpringBootWebSpring/Security/SpringBootWebSpringSecurityApplication.java
@@ -0,0 +1,13 @@
+package com.example.springboot.SpringBootWebSpring.Security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootWebSpringSecurityApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootWebSpringSecurityApplication.class, args);
+	}
+
+}
